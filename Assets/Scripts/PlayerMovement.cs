e1904d57455894e98ee641c9e368466d9f561692
ShrkeyAMB <124301900+ShrkeyAMB@users.noreply.github.com>
2024-03-03 17:14:22 -0800

    Part3

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,21 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    //type for rigidBody
     private Rigidbody rbPlayer;
+    private Vector3 direction = Vector3.zero;
+
+    //Player Variables
+    public float speed = 10f;
 
     private void Start()
     {
         rbPlayer = GetComponent<Rigidbody>();
+
     }
     private void Update()
     {
+        //player Movement
         float horMove = Input.GetAxis("Horizontal");
-        
-        rbPlayer.AddForce(new Vector3(horMove, 0, 0),ForceMode.Impulse);
+        float verMove = Input.GetAxis("Vertical");
+
+        direction = new Vector3(horMove,0 ,verMove);
 
 
+    }
+
+    private void FixedUpdate()
+    {
+        rbPlayer.AddForce(direction * speed,ForceMode.Force);
     }
 }
